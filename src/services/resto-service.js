1af47d000ca25f35b48951bc5eb772dc172dcc80
Imansaparov <Malikimansaparov@gmail.com>
2021-11-27 15:18:54 +0600

    update json-server patch

--- src/services/resto-service.js
+++ src/services/resto-service.js
@@ -1,5 +1,5 @@
 export default class RestoService{
-    _apiBase = 'http://localhost:3000';
+    _apiBase = 'https://my-json-server.typicode.com/malikImansaparov/restore-server/';
 
     async getResource(url) {
         const res = await fetch(`${this._apiBase}${url}`);
